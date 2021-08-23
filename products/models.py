0bead104ee689105285a05248f7bf69c2a6ed94f
Hikmat <Hikmatullah.ibrat2018@gmail.com>
2021-08-23 15:08:49 +0430

    git tracking for products templates

---
+++ products/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+import os
+import random
+
+# Create your models here.
+# remember that every model must be registered on admin.py
+
+
+
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath) # basename returns the final component of file
+    name , ext =os.path.splitext(base_name)
+    return name , ext
+
+
+# changing the filename and adding a random number to filename
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)     # filename is the type of file like jpg or png
+    new_filename = random.randint(1,39138493833)
+    # calling the upper function
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename = new_filename, ext = ext )
+    return 'product/{new_filename}/{final_filename}'.format(
+        new_filename = new_filename,
+        final_filename = final_filename
+    )
+
+#class ProductManager(models.Manager):
+  #  def get_by_id(self,id):
+   #     qs = self.get_queryset().filter(id = id) #Product.objects == self.get_queryset()
+    #    if qs.count() ==1:
+     #       return qs.first()
+      #  return None
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    # those two arguments are required on Decimal field and we can set null = True but default is better
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+
+    # we should use ImageField for just allowing images but it requires the Pillow 
+    # Pillow should be downloaded with python -m pip install Pillow
+    # After installing the Pillow run the makemigrations and migrate commands
+    # but still something this technic is not allowed for large file upload
+    image = models.FileField(upload_to= upload_image_path, null = True, blank = True)
+    
+   # object = ProductManager()
+
+    def __str__(self):
+        return self.title   
+
+
---
+++ products/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from django.http import Http404
+# Create your views here.
+from .models import Product
+from django.views.generic import ListView , DetailView
+
+
+#class based view for listing products
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = 'product/list.html'
+
+
+# Function base view for listing products:
+def product_list_view(request):
+    queryset = Product.objects.all()
+    """On 21st  line we can change the name of object_list to anything but it be object_list when we 
+    work with class based view but if we work with function based views not necessary to pass the 
+    data with anyname"""
+    Product_list = {
+        'object_list' : queryset
+    }
+    # we can also send the data to index.html on outer templates but not to other pages of out one
+    #return render(request , 'index.html',Product_list)
+    return render (request, 'product/list.html',Product_list)
+
+
+#class based detail view for listing products:
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'product/detail.html'
+
+
+    """This code is just for recognizing that how we can get the data with which name 
+    Which in this place it show (object) and that's why we retrieved the data with object : like
+    object.title or object.description"""
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        #context['object_list'] = queryset
+        return context
+
+
+
+# Function base detail view for listing products:
+def product_Detail_view(request,pk):
+    #queryset = Product.objects.all()
+    #queryset1 = Product.objects.get(id = pk)
+    try:
+        queryset1 = Product.objects.get(id = pk)
+    except Product.DoesNotExist:
+        print('No Product Here')
+        raise Http404('Product Does Not Exist have you seen')
+    except:
+        print('nothing for finally')
+    Product_list = {
+        
+        'object' : queryset1
+    }
+    # we can also send the data to index.html on outer templates but not to other pages of out one
+    #return render(request , 'index.html',Product_list)
+    return render (request, 'product/detail.html',Product_list)
+
+ 
